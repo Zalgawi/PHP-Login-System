53a944991f22a98e8a6b443789e97950dceeee9a
Zalgawi <36713303+Zalgawi@users.noreply.github.com>
2018-11-26 16:30:27 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
 <?php 
 
-echo "hello world";
+
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.24/css/uikit.min.css" />
+
+	<title>Page Title</title>
+	<base href="/"/>
+
+</head>
+<body>
+							
+<div class="uk-section uk-container ">
+	<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid></div>
+				<form class="uk-form-stacked">
+				    <div class="uk-margin">
+				        <label class="uk-form-label" for="form-stacked-text"><b>Email:</b></label>
+				        <div class="uk-form-controls">
+				            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="Insert Email">
+				        </div>
+				    </div>
+				    <div class="uk-margin">
+				        <label class="uk-form-label" for="form-stacked-text"><b>Password:</b></label>
+				        <div class="uk-form-controls">
+				            <input class="uk-input" id="form-stacked-text" type="Password" required='required' placeholder="Insert Password">
+				        </div>
+				    </div>
+				    <div class="uk-margin">
+				        <button class="uk-button uk-button-default" type="submit"><b>LOGIN</b></button>
+				    </div>
+			  	
+			  	</form>
+</div>
+
+<!-- jQuery is required -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.24/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.24/js/uikit-icons.min.js"></script>
+</body>
+</html>
